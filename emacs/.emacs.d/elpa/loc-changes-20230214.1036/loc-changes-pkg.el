2e0612a88da6d7214ddc3527cfc0cd2120864533
Shubhrakamal Saha <sahashubhrakamal@gmail.com>
2025-11-18 13:06:05 +0530

    Clean: Remove Emacs cache from git tracking

--- emacs/.emacs.d/elpa/loc-changes-20230214.1036/loc-changes-pkg.el
+++
@@ -1,9 +0,0 @@
-;; -*- no-byte-compile: t; lexical-binding: nil -*-
-(define-package "loc-changes" "20230214.1036"
-  "Keep track of positions even after buffer changes."
-  ()
-  :url "https://github.com/rocky/emacs-loc-changes"
-  :commit "622371e432f50626aaac82f8ee2841f71685b0fb"
-  :revdesc "622371e432f5"
-  :authors '(("Rocky Bernstein" . "rocky@gnu.org"))
-  :maintainers '(("Rocky Bernstein" . "rocky@gnu.org")))
